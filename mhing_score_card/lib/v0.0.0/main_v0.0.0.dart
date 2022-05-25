4b362f8a398a58487679a8a2d8fea6f2e193766c
Aztal-Samoht <thatguysquig@gmail.com>
2022-05-25 17:54:22 -0400

    got first routes set up

--- mhing_score_card/lib/v0.0.0/main_v0.0.0.dart
+++ mhing_score_card/lib/v0.0.0/main_v0.0.0.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mhing_score_card/v0.0.0/screens/home_screen.dart';
 
 class MyAppV0_0_0 extends StatelessWidget {
   const MyAppV0_0_0({Key? key}) : super(key: key);
@@ -6,7 +7,11 @@ class MyAppV0_0_0 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:
+      title: 'Mhingscore Card!',
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+      },
     );
   }
 }
---
+++ mhing_score_card/lib/v0.0.0/screens/scorecard_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScorecardScreen extends StatelessWidget {
+  static String id = '/scorecard';
+  const ScorecardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
