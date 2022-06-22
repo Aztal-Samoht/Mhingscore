c8f0e9f1029b531fcdf7bed4e9d3d51ee38d848d
Aztal-Samoht <thatguysquig@gmail.com>
2022-06-21 20:00:09 -0500

    ready to merge into main

--- mhing_score_card/lib/res/ad_helper.dart
+++ mhing_score_card/lib/res/ad_helper.dart
@@ -34,6 +34,46 @@ class AdHelper {
     }
   }
 
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-5934630788997149~7422978619";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4411468910";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get rewardedInterstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/1033173712";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4411468910";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  ///*
+  ///
+  /// merge before uploading the thing!!!!!!
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+
+  static String get testRewardedInterstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/5354046379";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4411468910";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
   static String get testInterstitialAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-3940256099942544/1033173712";
